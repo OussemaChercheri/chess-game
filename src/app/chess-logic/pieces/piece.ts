7d1ed8ebf1263d122736b2d292bea44586ff6556
Oussema Chercheri <oussama.chercheri@tek-up.de>
2024-05-30 04:22:19 +0100

    add abstract class pieces

---
+++ src/app/chess-logic/pieces/piece.ts
@@ -0,0 +1,16 @@
+import { Color, Coords, FENChar } from '../models';
+
+export abstract class Piece {
+  protected abstract _FENChar: FENChar;
+  protected abstract _directions: Coords[];
+
+  constructor(protected _color: Color) {}
+
+  public get FENCHAR(): FENChar {
+    return this._FENChar;
+  }
+
+  public get color(): Color {
+    return this._color;
+  }
+}
